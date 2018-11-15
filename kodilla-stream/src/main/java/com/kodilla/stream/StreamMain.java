ba6e590c7167bb04755715c9f69ec0133cb56e30
szymonstuszek <szymonstuszek@gmail.com>
2018-11-15 11:24:31 +0100

    Task 7.3 - correction with getting the user age

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,9 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
-import com.kodilla.stream.person.People;
 
 import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -18,7 +13,7 @@ public class StreamMain {
 
         Map<Integer, ForumUser> resultMapOfUsers = forum.getForumUserList().stream()
                 .filter(user -> user.getGender() == 'M')
-                .filter(user -> user.getDateOfBirth().isBefore(LocalDate.of(1998,11,12)))
+                .filter(user -> (LocalDate.now().getYear() - user.getDateOfBirth().getYear()) >= 20)
                 .filter(user -> user.getNumberOfPublishedPosts() > 0)
                 .collect(Collectors.toMap(ForumUser::getId, user -> user));
 
