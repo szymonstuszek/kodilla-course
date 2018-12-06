7e1ff712bfa923297d416c92d0e1893abebc199b
szymonstuszek <szymonstuszek@gmail.com>
2018-12-06 11:02:13 +0100

    completed task 102

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +19,7 @@ public class CalculatorTestSuite {
         double b = 5.0;
 
         ApplicationContext context =
-                new AnnotationConfigApplicationContext();
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
         Calculator calculator = context.getBean(Calculator.class);
 
         //When
@@ -27,6 +28,10 @@ public class CalculatorTestSuite {
         double mul = calculator.mul(a, b);
         double div = calculator.div(a, b);
 
-
+        //Then
+        Assert.assertEquals(25.0, add, 0.001);
+        Assert.assertEquals(15.0, sub, 0.001);
+        Assert.assertEquals(100.0, mul, 0.001);
+        Assert.assertEquals(4.0, div, 0.001);
     }
 }
