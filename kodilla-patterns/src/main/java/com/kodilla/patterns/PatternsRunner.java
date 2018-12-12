0a6079f0d136d0084e2de56e791cb3ee072f95d2
szymonstuszek <36398500+szymonstuszek@users.noreply.github.com>
2018-12-12 14:55:17 +0100

    Update PatternsRunner.java

--- kodilla-patterns/src/main/java/com/kodilla/patterns/PatternsRunner.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/PatternsRunner.java
@@ -3,5 +3,6 @@ package com.kodilla.patterns;
 public class PatternsRunner {
     public static void main(String[] args) {
         System.out.println("welcome to the module 11 - patterns.");
+        System.out.println("Added a comment online");
     }
 }
