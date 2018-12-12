ed7df598550f34afb7111143bb40dcf402a67d5e
szymonstuszek <szymonstuszek@gmail.com>
2018-12-12 16:52:14 +0100

    Task111 - finished subtask 1: created the test

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,11 +1,21 @@
 package com.kodilla.patterns.singleton;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LoggerTestSuite {
 
     @Test
     public void testGetLastLog() {
+        //Given
+        Logger logger = new Logger();
+        String firstLog = "First log test";
 
+        //When
+        logger.log(firstLog);
+        String retrievedLog = logger.getLastLog();
+
+        //Then
+        Assert.assertEquals(firstLog, retrievedLog);
     }
 }
