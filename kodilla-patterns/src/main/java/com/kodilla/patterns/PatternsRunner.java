a2821dbbbbcc1e06851301def0db684338501aca
szymonstuszek <szymonstuszek@gmail.com>
2018-12-12 14:58:18 +0100

    Merge confilct test

--- kodilla-patterns/src/main/java/com/kodilla/patterns/PatternsRunner.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/PatternsRunner.java
@@ -4,5 +4,6 @@ public class PatternsRunner {
     public static void main(String[] args) {
         System.out.println("welcome to the module 11 - patterns.");
         System.out.println("Added a comment online");
+        System.out.println("Added locally");
     }
 }
