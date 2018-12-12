ee30c08e6c01a7e50008ae35f8291b6bf5c6be04
szymonstuszek <szymonstuszek@gmail.com>
2018-12-12 15:01:30 +0100

    cleaned after testing, ready to work further

--- kodilla-patterns/src/main/java/com/kodilla/patterns/PatternsRunner.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/PatternsRunner.java
@@ -3,7 +3,5 @@ package com.kodilla.patterns;
 public class PatternsRunner {
     public static void main(String[] args) {
         System.out.println("welcome to the module 11 - patterns.");
-        System.out.println("Added a comment online");
-        System.out.println("Added final fix");
     }
 }
