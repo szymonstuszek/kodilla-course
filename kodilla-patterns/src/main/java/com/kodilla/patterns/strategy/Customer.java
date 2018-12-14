3839a8a4bd8d47d76a9cd915f909bce7198d3c53
szymonstuszek <szymonstuszek@gmail.com>
2018-12-14 06:47:12 +0100

    Added Customer Class

--- kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/Customer.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/Customer.java
@@ -1,4 +1,13 @@
 package com.kodilla.patterns.strategy;
 
 public class Customer {
+    final private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
