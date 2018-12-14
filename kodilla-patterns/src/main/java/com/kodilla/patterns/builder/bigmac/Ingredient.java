6145b5669587cc17f9a0c7ae3f057d4bcbc25c5d
szymonstuszek <szymonstuszek@gmail.com>
2018-12-14 15:04:19 +0100

    Finished task 115

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Ingredient.java
@@ -0,0 +1,12 @@
+package com.kodilla.patterns.builder.bigmac;
+
+public class Ingredient {
+    public static final String SALAD = "SALAD";
+    public static final String ONION = "ONION";
+    public static final String BECON = "BECON";
+    public static final String CUCUMBER = "CUCUMBER";
+    public static final String CHILLI = "CHILLI";
+    public static final String MUSHROOMS = "MUSHROOMS";
+    public static final String SHRIMPS = "SHRIMPS";
+    public static final String CHEESE = "CHEESE";
+}
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Sauce.java
@@ -0,0 +1,7 @@
+package com.kodilla.patterns.builder.bigmac;
+
+public class Sauce {
+    public static final String STANDARD = "STANDARD";
+    public static final String THOUSAND_ISLANDS = "THOUSAND_ISLANDS";
+    public static final String BARBECUE = "BARBECUE";
+}
