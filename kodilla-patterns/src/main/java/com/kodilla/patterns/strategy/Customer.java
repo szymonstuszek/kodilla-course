f52a2da123b246644d25a02b302d1699cde5e97f
szymonstuszek <szymonstuszek@gmail.com>
2018-12-14 07:02:07 +0100

    Added predictors and customer implementations

--- kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/Customer.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/Customer.java
@@ -2,6 +2,7 @@ package com.kodilla.patterns.strategy;
 
 public class Customer {
     final private String name;
+    protected BuyPredictor buyPredictor;
 
     public Customer(String name) {
         this.name = name;
@@ -9,5 +10,13 @@ public class Customer {
 
     public String getName() {
         return name;
+    }
+
+    public String predict() {
+        return buyPredictor.predictWhatToBuy();
+    }
+
+    public void setBuyingStrategy(BuyPredictor buyPredictor) {
+        this.buyPredictor = buyPredictor;
     }
 }
